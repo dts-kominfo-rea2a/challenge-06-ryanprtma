94c007e2e3fd8d458518ffc59084372d83d233a8
ryanprtma <ryanapratama16@gmail.com>
2022-06-13 17:26:15 +0700

    update

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -18,58 +18,62 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = (fnCallback) => {
-  fs.readFile(
-    file1,
-    {
-      encoding: 'utf8',
-    },
-    (err, data) => {
-      if (err) {
-        return console.log(err);
-      }
-      const arrayOfObject1 = JSON.parse(data);
-      const data1 = fnCallback(err, arrayOfObject1.message.substring(5, 10))
-      fs.readFile(
-        file2,
-        {
-          encoding: 'utf8',
-        },
-        (err, data) => {
-          if (err) {
-            return console.log(err);
-          }
-          const arrayOfObject2 = JSON.parse(data);
-          const data2 = fnCallback(err, arrayOfObject2[0].message.substring(5, 10))
-          fs.readFile(
-            file3,
-            {
-              encoding: 'utf8',
-            },
-            (err, data) => {
-              if (err) {
-                return console.log(err);
-              }
-              const arrayOfObject3 = JSON.parse(data);
-              const data3 = fnCallback(err, arrayOfObject3[0].data.message.substring(5, 10))
-              const arr = [data1, data2, data3]
-              return console.log(arr)
-            }
-          )
-        }
-      )
-    }
-  )
+const bacaData = (err, data) => {
+  if (err) {
+    fnCallback(err, data)
+  }
+  fnCallback(null, data)
 }
 
 const fnCallback = (err, data) => {
+  // let arr = []
   if (err) {
     return console.log('Ada terjadi error: ' + err);
   }
   return data
 }
 
-bacaData(fnCallback)
+bacaData(fs.readFile(
+  file1,
+  {
+    encoding: 'utf8',
+  },
+  (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    const arrayOfObject1 = JSON.parse(data);
+    const data1 = fnCallback(err, arrayOfObject1.message.substring(5, 10))
+    fs.readFile(
+      file2,
+      {
+        encoding: 'utf8',
+      },
+      (err, data) => {
+        if (err) {
+          return console.log(err);
+        }
+        const arrayOfObject2 = JSON.parse(data);
+        const data2 = fnCallback(err, arrayOfObject2[0].message.substring(5, 10))
+        fs.readFile(
+          file3,
+          {
+            encoding: 'utf8',
+          },
+          (err, data) => {
+            if (err) {
+              return console.log(err);
+            }
+            const arrayOfObject3 = JSON.parse(data);
+            const data3 = fnCallback(err, arrayOfObject3[0].data.message.substring(5, 10))
+            const arr = [data1, data2, data3]
+            return console.log(arr)
+          }
+        )
+      }
+    )
+  }
+))
 
 
 // ! JANGAN DIMODIFIKASI
